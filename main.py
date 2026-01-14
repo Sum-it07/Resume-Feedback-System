1319edda717fda9f9ff2f55144263ff3499b2f65
Sumit Shrestha <sumitstha2060@gmail.com>
2026-01-14 16:08:10 +0530

    logic for skill extraction

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.analysis.skill_extraction import load_skill,extract_skill
 from pathlib import Path
 def load_text(path):
     return Path(path).read_text(encoding="utf-8")
@@ -5,13 +6,20 @@ def load_text(path):
 def main():
     resume=load_text("data/resume/resume1.txt")
     jd=load_text("data/job_description/jd1.txt")
+    skill=load_skill("data/skill.txt")
 
-    print("==AI SYSTEM==")
+    resume_skill=extract_skill(resume, skill)
+    jd_skill=extract_skill(jd, skill)
     print("==Your Resume==")
     print(resume)
 
     print("==Job description==")
     print(jd)
+
+    print("==resume skill")
+    print(resume_skill)
+    print("==jd skill")
+    print(jd_skill)
 
 if __name__== "__main__":
     main()

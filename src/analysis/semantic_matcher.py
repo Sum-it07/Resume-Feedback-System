600ae5fb16761b1ee58f88e760f793f9d5bb7419
Sumit Shrestha <sumitstha2060@gmail.com>
2026-02-19 11:04:59 +0530

    "test deploy"

--- src/analysis/semantic_matcher.py
+++ src/analysis/semantic_matcher.py
@@ -1,7 +1,36 @@
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
+# from sentence_transformers import SentenceTransformer
+# from sklearn.metrics.pairwise import cosine_similarity
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# def semantic_candidates(
+#     resume_skill,
+#     required_skill,
+#     threshold=0.6,
+#     top_k=3
+# ):
+#     resume_emb = model.encode(resume_skill)
+#     required_emb = model.encode(required_skill)
+
+#     similarity = cosine_similarity(required_emb, resume_emb)
+
+#     evidence = {}
+
+#     for i, jd_skill in enumerate(required_skill):
+#         scored = []
+
+#         for j, score in enumerate(similarity[i]):
+#             if score >= threshold:
+#                 scored.append(
+#                     (resume_skill[j], round(float(score), 2))
+#                 )
+
+#         if scored:
+#             # sort descending by similarity
+#             scored.sort(key=lambda x: x[1], reverse=True)
+#             evidence[jd_skill] = scored[:top_k]
+
+#     return evidence
 
 def semantic_candidates(
     resume_skill,
@@ -9,25 +38,4 @@ def semantic_candidates(
     threshold=0.6,
     top_k=3
 ):
-    resume_emb = model.encode(resume_skill)
-    required_emb = model.encode(required_skill)
-
-    similarity = cosine_similarity(required_emb, resume_emb)
-
-    evidence = {}
-
-    for i, jd_skill in enumerate(required_skill):
-        scored = []
-
-        for j, score in enumerate(similarity[i]):
-            if score >= threshold:
-                scored.append(
-                    (resume_skill[j], round(float(score), 2))
-                )
-
-        if scored:
-            # sort descending by similarity
-            scored.sort(key=lambda x: x[1], reverse=True)
-            evidence[jd_skill] = scored[:top_k]
-
-    return evidence
+    return {}
